13a1b19f035b44c1fee9c83cdd98b1f0e2311585
Collins Muriuki <murerwacollins@gmail.com>
2020-09-16 14:36:18 +0300

    Access url parameters using web::Path

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+#[get("/{id}/{name}/test")]
+async fn test(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -20,6 +25,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(test)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
