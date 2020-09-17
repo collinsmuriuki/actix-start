5e4e0554e35775fa52ff8e8ddef2b82d6f8c418c
Collins Muriuki <murerwacollins@gmail.com>
2020-09-17 16:00:31 +0300

    2020-09-17_16-00

--- src/api/config.rs
+++ src/api/config.rs
@@ -1,10 +1,22 @@
-use actix_web::{web,HttpResponse};
+use actix_web::{web,HttpResponse,Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize,Debug)]
+struct User {
+    username: String,
+    password: String
+}
+
+async fn login(info: web::Json<User>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Welcome {} and your password is {}", info.username, info.password))
+}
 
 // this function could be located in a different module
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/config")
             .route(web::get().to(|| HttpResponse::Ok().body("config")))
-            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed()))
+            .route(web::post().to(login)),
     );
 }
