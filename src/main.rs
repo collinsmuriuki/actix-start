596a532970c66d2af4c4b705de6ddd9a68df959f
Collins Muriuki <murerwacollins@gmail.com>
2020-09-17 14:24:35 +0300

    2020-09-17_14-24

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use actix_web::{get,HttpServer,App,HttpResponse,web};
 
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+
+    format!("Hello {}!", app_name) // <- response with app_name
+}
+
 #[get("/show")]
 async fn show_users() -> HttpResponse {
     HttpResponse::Ok().body("Show users")
@@ -13,10 +26,15 @@ async fn user_detail(path: web::Path<(u32,)>) -> HttpResponse {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/users")
-                .service(show_users)
-                .service(user_detail),
+        App::new()
+            .data(AppState {
+                app_name: String::from("This is a state value"),
+            })
+            .service(
+                web::scope("/users")
+                    .service(index)
+                    .service(show_users)
+                    .service(user_detail),
         ).default_service(web::route().to(|| HttpResponse::MethodNotAllowed()))
     })
     .bind("127.0.0.1:8080")?
