5bfcb8e7b04345f7fc170fd417a36c28665b78c5
Collins Muriuki <murerwacollins@gmail.com>
2020-09-17 18:03:34 +0300

    2020-09-17_18-03

--- src/api/config.rs
+++ src/api/config.rs
@@ -1,14 +1,32 @@
-use actix_web::{web,HttpResponse,Responder};
-use serde::Deserialize;
+use actix_web::{web,HttpResponse,HttpRequest,Responder,Error};
+use serde::{Deserialize,Serialize};
+use serde_json::to_string;
+use futures::future::{ready, Ready};
 
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize,Serialize,Debug)]
 struct User {
     username: String,
     password: String
 }
 
+impl Responder for User {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse,Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = to_string(&self).unwrap();
+
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)
+        ))
+    }
+}
+
 async fn login(info: web::Json<User>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Welcome {} and your password is {}", info.username, info.password))
+    let username = info.username.clone();
+    let password = info.password.clone();
+    User { username, password }
 }
 
 // this function could be located in a different module
